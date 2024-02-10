49fb6f1d3554d13b8d21b23647edcf2dd34a589f
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-02-10 15:34:17 +0200

    minor changes and made template of AboutUs section

--- projects/water-well-project/script.js
+++ projects/water-well-project/script.js
@@ -9,8 +9,21 @@ let toggleTheme = () => {
     const lightThemePath = 'source/header/configs/light.png';
     const darkThemePath = 'source/header/configs/dark.png';
     let themeIcon = document.getElementById("themeIcon");
-    
+
     themeIcon.src.includes(lightThemePath)
     ? themeIcon.src = darkThemePath
     : themeIcon.src = lightThemePath;
 }
+
+// let toggleMenu = () => {
+//     let linksProp = document.getElementsByClassName("links")[0].style
+//     if(linksProp.display === '' || linksProp.display === 'none') {
+//         linksProp.display = 'block'
+//         linksProp.position = 'absolute'
+//         linksProp.top = '10vw'
+//         linksProp.left = '10vw'
+//     } else {
+//         linksProp.display = 'none'
+//     }
+
+// }
