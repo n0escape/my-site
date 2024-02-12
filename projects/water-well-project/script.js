aea7ac037cc4c8fd3489a9ae5092d6c3cef2b084
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-02-12 14:09:44 +0200

    added some fixed added pageUp button and added ourWorks block

--- projects/water-well-project/script.js
+++ projects/water-well-project/script.js
@@ -1,3 +1,15 @@
+window.addEventListener('scroll', function() {
+    var footer = document.querySelector('footer');
+    var bottomLeft = document.getElementsByClassName('pageUp')[0];
+    var footerOffset = footer.getBoundingClientRect().top - window.innerHeight;
+
+    if (footerOffset <= 0) {
+        bottomLeft.style.bottom = Math.abs(footerOffset) + 'px';
+    } else {
+        bottomLeft.style.bottom = '10px';
+    }
+});
+
 let toggleLanguageBox = () => {
     let styleProps = document.getElementsByClassName("hiddenLanguageBox")[0].style
     styleProps.display === '' || styleProps.display === 'none'
