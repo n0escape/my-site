7edcf8a9cb8fb934bffcc4486800bb17fa8936bd
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-02-19 01:27:20 +0200

    fixed footer

--- projects/water-well-project-react/src/generalComponents/Footer/Footer.jsx
+++ projects/water-well-project-react/src/generalComponents/Footer/Footer.jsx
@@ -8,127 +8,126 @@ import googleIcon from "./../../assets/socialMedias/google.png"
 
 const Footer = () => {
     return (
-        <footer id="pageFooter">
-          <div class="container">
-            <div class="logo">
-                <img class="logoIcon" 
-                  src={companyLogo} 
-                  alt="Company logo" width="150px" />
-                <p class="adress">
-                  Україна м. Харків вул. Полтавський шлях 10, офіс 302
-                </p>
-            </div>
-            <div class="services">
-              <h2>Наші посуги</h2>
-              <ul class="servicesList">
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    Послуга
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="ourContacts">
-              <h2>Наші контакти</h2>
-              <ul class="phones">
-                <li>
-                  <a href="#.">
-                    +380-xxx-xx-xx
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    +380-xxx-xx-xx
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    +380-xxx-xx-xx
-                  </a>
-                </li>
-              </ul>
-              <ul class="socialMedias">
-                <li>
-                  <a href="#.">
-                    <img src={facebookIcon} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    <img src={instagramIcon} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    <img src={telegramIcon} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    <img src={googleIcon} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    <img src={googleIcon} alt="" />
-                  </a>
-                </li>
-                <li>
-                  <a href="#.">
-                    <img src={googleIcon} alt="" />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class=""></div>
+      <footer id="pageFooter">
+        <div class="container">
+          <div class="logo">
+              <img class="logoIcon" 
+                src={companyLogo} 
+                alt="Company logo" width="150px" />
+              <p class="adress">
+                Україна м. Харків вул. Полтавський шлях 10, офіс 302
+              </p>
           </div>
-      </footer>
-    )
+          <div class="services">
+            <h2>Наші посуги</h2>
+            <ul class="servicesList">
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  Послуга
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="ourContacts">
+            <h2>Наші контакти</h2>
+            <ul class="phones">
+              <li>
+                <a href="#.">
+                  +380-xxx-xx-xx
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  +380-xxx-xx-xx
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  +380-xxx-xx-xx
+                </a>
+              </li>
+            </ul>
+            <ul class="socialMedias">
+              <li>
+                <a href="#.">
+                  <img src={facebookIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  <img src={instagramIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  <img src={telegramIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  <img src={googleIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  <img src={googleIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a href="#.">
+                  <img src={googleIcon} alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </footer>
+  )
 }
 
 export default Footer;
